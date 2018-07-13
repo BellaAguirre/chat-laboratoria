1ac51dc4f33f8b98b07dfb9783f1ee4b2644b748
BellaAguirre <yunoshe1@gmail.com>
2018-07-13 05:39:41 -0500

    login y logout listo

--- app.js
+++ app.js
@@ -59,6 +59,30 @@ const logout = () => {
   })
   .catch();
 }
+const logginFacebook = () => {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  //provider.addScope(user_birthday)
+  provider.setCustomParameters({
+    'display': 'popup'
+  });
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+}
 const btnLogin = document.getElementById('btnloggin');
 const btnRegister = document.getElementById('btnregister');
 const btnLoggout = document.getElementById('btnloggout');
